1e2b3380dbcbce35b8da62a57aedbc13eda18480
Larry Diamond <1066589+larrydiamond@users.noreply.github.com>
2024-03-23 21:20:07 -0400

    changed package missed some references

--- src/test/java/com/ldiamond/archunittest/TestArchitectureUnitTest.java
+++ src/test/java/com/ldiamond/archunittest/TestArchitectureUnitTest.java
@@ -1,3 +1,22 @@
+/*
+ * #%L
+ * Standardized ArchUnit tests
+ * %%
+ * Copyright (C) 2024 Larry Diamond
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
 package com.ldiamond.archunittest;
 
 import org.junit.jupiter.api.Test;
@@ -18,11 +37,13 @@ class TestArchitectureUnitTest {
 
     @Test void testpackageFailsRulesetWithExclusionForVectorButNotHashtable () {
         assertThrowsExactly (AssertionError.class, () -> {
-            ArchitectureUnitTest.testArchitecture(Arrays.asList(ArchitectureRule.USE_ARRAYLIST_INSTEAD_OF_VECTOR), "com.ldiamond.archcommon.testpackage");
+            ArchitectureUnitTest.testArchitecture(Arrays.asList(ArchitectureRule.USE_ARRAYLIST_INSTEAD_OF_VECTOR), 
+            "com.ldiamond.archunittest.testpackage");
         });
     }
 
     @Test void testpackageSucceedsRulesetWithExclusionForVectorAndHashtable () {
-        ArchitectureUnitTest.testArchitecture(Arrays.asList(ArchitectureRule.USE_ARRAYLIST_INSTEAD_OF_VECTOR, ArchitectureRule.USE_HASHMAP_INSTEAD_OF_HASHTABLE), "com.ldiamond.archcommon.testpackage");
+        ArchitectureUnitTest.testArchitecture(Arrays.asList(ArchitectureRule.USE_ARRAYLIST_INSTEAD_OF_VECTOR, ArchitectureRule.USE_HASHMAP_INSTEAD_OF_HASHTABLE), 
+        "com.ldiamond.archunittest.testpackage");
     }
 }
